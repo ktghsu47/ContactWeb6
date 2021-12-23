3d85843c64aaa048efbbf2c52fd7a84f834432a2
Kuo-Tung Hsu <ktghsu@hotmail.com>
2021-12-22 21:15:39 -0500

    first updates

--- ContactWeb6/Data/ApplicationDbContext.cs
+++ ContactWeb6/Data/ApplicationDbContext.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace ContactWeb6.Data
-{
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+namespace ContactWeb6.Data {
+    public class ApplicationDbContext : IdentityDbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     }
 }
